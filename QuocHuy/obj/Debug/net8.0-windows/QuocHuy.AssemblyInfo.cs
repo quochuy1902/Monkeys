661ffe88d64938d83eb2ddd20c80f445c8bbdbff
quochuy1902 <nguyenquochuy.1902k4@gmail.com>
2025-04-11 11:09:42 +0700

    Thêm unit test kiểm tra chức năng tìm bãi đỗ

--- QuocHuy/obj/Debug/net8.0-windows/QuocHuy.AssemblyInfo.cs
+++ QuocHuy/obj/Debug/net8.0-windows/QuocHuy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuocHuy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bdb6b8c0b46805f28cdad9e47ae3ce7421ece84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b2557d824180f38ed3ffe310ad6f110231499a")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuocHuy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuocHuy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
