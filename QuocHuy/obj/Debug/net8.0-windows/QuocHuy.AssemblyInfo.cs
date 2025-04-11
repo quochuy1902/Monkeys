41ba4f4423939ec8e0e2d9beacedeb958221cae8
quochuy1902 <nguyenquochuy.1902k4@gmail.com>
2025-04-11 11:20:06 +0700

    Giao diện xem lịch sử và filter theo tháng

--- QuocHuy/obj/Debug/net8.0-windows/QuocHuy.AssemblyInfo.cs
+++ QuocHuy/obj/Debug/net8.0-windows/QuocHuy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuocHuy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c19dcd3e71a240f4fa201fa1a50f25f1b2f2bfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b8f153f64abac7f2d41e0780b898f3fad5d280")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuocHuy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuocHuy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
